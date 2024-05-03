4904f72d325ff3075cbc1015bdc7d13bd71b5ef0
unknown <isaferroortiz@gmail.com>
2024-05-02 21:14:19 -0300

    at 0.1

---
+++ js/ativ-1.js
@@ -0,0 +1,48 @@
+alert("mensagem aleatoria uoouoo");
+
+var nome = "isa";
+
+console.log(nome);
+
+let num1 = 10;
+let num2 = 5;
+let a = 20;
+let b = 4;
+console.log(num1 + num2),
+
+console.log(num1 - num2),
+
+console.log(num1 * num2),
+
+console.log(num1 / num2),
+
+console.log(num1 ** num2);
+
+console.log(5 == "5");
+
+console.log(5 === 5);
+
+console.log(5 === "5");
+
+console.log(5 > 7);
+
+console.log(5 !== 5);
+
+console.log(5 !== 7);
+
+console.log(a > 0 && b > 3);
+
+console.log(a > 0 || b > 5);
+
+console.log(a > 10 && ! b < 5);
+
+let resposta = confirm("will you given me your heart?")
+console.log (resposta);
+
+
+
+/*
+|| ou
+&& 
+! não
+*/
